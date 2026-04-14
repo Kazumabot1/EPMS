67152285d1a3fe6f41416e4fc6519579f8404568
nomore-yyp317 <paingyeyint337@gmail.com>
2026-04-14 20:46:04 +0630

    feedback module

---
+++ src/main/java/com/epms/entity/FeedbackSection.java
@@ -0,0 +1,23 @@
+package com.epms.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "feedback_section")
+public class FeedbackSection {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long feedbackSectionId;
+
+    private String title;
+
+    private Integer orderNo;
+
+    @ManyToOne
+    @JoinColumn(name = "feedback_form_id")
+    private FeedbackForm feedbackForm;
+
+    @OneToMany(mappedBy = "section", cascade = CascadeType.ALL)
+    private List<FeedbackQuestion> questions;
+}
